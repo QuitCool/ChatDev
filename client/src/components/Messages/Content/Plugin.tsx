a1ce4d967a5991408aa10555590b283a15603f79
Quitcool <mo7senhajjaj@gmail.com>
2023-11-23 20:33:10 +0200

    zxx

--- client/src/components/Messages/Content/Plugin.tsx
+++ client/src/components/Messages/Content/Plugin.tsx
@@ -112,7 +112,7 @@ const Plugin: React.FC<PluginProps> = ({ plugin }) => {
                     lang={
                       latestPlugin ? `RESPONSE FROM ${latestPlugin?.toUpperCase()}` : 'RESPONSE'
                     }
-                    codeChildren={formatJSON('FINISH')}
+                    codeChildren={('FINISH')}
                     plugin={true}
                     classProp="max-h-[450px]"
                   />
