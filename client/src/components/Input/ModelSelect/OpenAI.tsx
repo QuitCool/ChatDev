5377bf9a38e6e02b2584e12c72c7dbdf16e627d3
Quitcool <mo7senhajjaj@gmail.com>
2023-11-24 03:06:29 +0200

    zze

--- client/src/components/Input/ModelSelect/OpenAI.tsx
+++ client/src/components/Input/ModelSelect/OpenAI.tsx
@@ -13,9 +13,9 @@ export default function OpenAI({
 
   // Mapping of model internal names to display names
   const modelNameMappings = {
-    'gpt-4-0613': 'GPT 4',
     'gpt-4-1106-preview': 'GPT 4 Turbo',
     'gpt-4-vision-preview': 'GPT 4 Vision',
+    'gpt-4-0613': 'GPT 4',
     'gpt-3.5-turbo-1106': 'GPT 3.5 Turbo',
   };
 
