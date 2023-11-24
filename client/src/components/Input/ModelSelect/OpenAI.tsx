291a39f46969859180b597a006c5360ed10d79ac
Quitcool <mo7senhajjaj@gmail.com>
2023-11-24 03:15:16 +0200

    sxz

--- client/src/components/Input/ModelSelect/OpenAI.tsx
+++ client/src/components/Input/ModelSelect/OpenAI.tsx
@@ -1,35 +1,28 @@
-import { SelectDropDown, SelectDropDownPop } from '~/components/ui';
-import type { TModelSelectProps } from '~/common';
+import { SelectDropDown } from '~/components/ui';
 import { cn, cardStyle } from '~/utils/';
+import type { TModelSelectProps } from '~/common';
 
-export default function OpenAI({
-  conversation,
-  setOption,
-  models,
-  showAbove = true,
-  popover = true,
-}: TModelSelectProps) {
-  const Menu = popover ? SelectDropDownPop : SelectDropDown;
-
-  // Mapping of model internal names to display names
-  const modelNameMappings = {
+export default function OpenAI({ conversation, setOption, models }: TModelSelectProps) {
+  const modelDisplayNames = {
     'gpt-4-1106-preview': 'GPT 4 Turbo',
-    'gpt-4-vision-preview': 'GPT 4 Vision',
     'gpt-4-0613': 'GPT 4',
     'gpt-3.5-turbo-1106': 'GPT 3.5 Turbo',
+    // Add other model mappings here if necessary
   };
 
-  // Function to alias model names
-  const aliasModelName = (modelName) => {
-    return modelNameMappings[modelName] || modelName;
+  const getModelDisplayName = (modelValue) => modelDisplayNames[modelValue] || modelValue;
+
+  const handleModelChange = (selectedDisplayName) => {
+    const selectedValue = Object.keys(modelDisplayNames).find(key => modelDisplayNames[key] === selectedDisplayName) || selectedDisplayName;
+    setOption('model')(selectedValue);
   };
 
   return (
-    <Menu
-      value={aliasModelName(conversation?.model ?? '')}
-      setValue={setOption('model')}
-      availableValues={models.map(model => aliasModelName(model))}
-      showAbove={showAbove}
+    <SelectDropDown
+      value={getModelDisplayName(conversation?.model ?? '')}
+      setValue={handleModelChange}
+      availableValues={models.map(getModelDisplayName)}
+      showAbove={true}
       showLabel={false}
       className={cn(
         cardStyle,
