18c0b5b9192410870e3f1d7efba75cd36dd331fa
Quitcool <mo7senhajjaj@gmail.com>
2023-12-02 01:42:23 +0200

    mintance

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
               rel="noreferrer"
               className="underline"
             >
-              {config?.appTitle || 'ChatGPT'} <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/800px-Flag_of_Egypt.svg.png" alt="Egyptian Flag" /> v1.12.2023
+              {config?.appTitle || 'ChatGPT'} <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/800px-Flag_of_Egypt.svg.png"  style={{ height: '1em' }} /> v1.12.2023
             </a>
             {' - '} {localize('com_ui_new_footer')}
           </>
