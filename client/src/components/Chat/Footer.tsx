715b2fe74186ac11de0f5349c95ab542d3f59c12
Quitcool <mo7senhajjaj@gmail.com>
2023-12-02 02:52:40 +0200

    logo fix try num 7.

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useGetStartupConfig } from 'librechat-data-provider';
 import { useLocalize } from '~/hooks';
 
@@ -5,9 +6,17 @@ export default function Footer() {
   const { data: config } = useGetStartupConfig();
   const localize = useLocalize();
 
+  const egyptianFlagImage = (
+    <img 
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/800px-Flag_of_Egypt.svg.png" 
+      alt="Egyptian Flag" 
+      style={{ height: '1em', marginLeft: '0.5em', marginRight: '0.5em' }} 
+    />
+  );
+
   return (
     <div className="relative px-2 py-2 text-center text-xs text-gray-600 dark:text-gray-300 md:px-[60px]">
-      <span>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         {typeof config?.customFooter === 'string' ? (
           config.customFooter
         ) : (
@@ -20,12 +29,10 @@ export default function Footer() {
             >
               {config?.appTitle || 'ChatGPT'} v1.12.2023
             </a>
-            {' - '}
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/800px-Flag_of_Egypt.svg.png" alt="Egyptian Flag" style={{ height: '1em', marginLeft: '0.5em', marginRight: '0.5em' }} />
-            {localize('com_ui_new_footer')}
+            {' - '}. {egyptianFlagImage} {localize('com_ui_new_footer')}
           </>
         )}
-      </span>
+      </div>
     </div>
   );
 }
