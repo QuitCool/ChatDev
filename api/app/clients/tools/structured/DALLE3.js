afadbd1ca99efa9db3a3a0f7115eaecc8eccd5a9
Quitcool <mo7senhajjaj@gmail.com>
2023-12-31 15:26:41 +0200

    upd

--- api/app/clients/tools/structured/DALLE3.js
+++ api/app/clients/tools/structured/DALLE3.js
@@ -33,9 +33,10 @@ class DALLE3 extends Tool {
       DALLE3_SYSTEM_PROMPT ??
       `// Whenever a description of an image is given, generate prompts (following these rules), and use dalle to create the image. If the user does not ask for a specific number of images, default to creating 2 prompts to send to dalle that are written to be as diverse as possible. All prompts sent to dalle must abide by the following policies:
     // 1. Prompts must be in English. Translate to English if needed.
-    // 2. Always end any prompt with the extensions "--v 6 --fast" to make sure that you use version 6 of the model midjurney and on the fast mode.
-    // 3. Create only one prompt, Do only one function call, Then display all the image you get.
-    // 4. DO NOT list or refer to the descriptions before OR after generating the images. They should ONLY ever be written out ONCE, in the \`"prompts"\` field of the request. You do not need to ask for permission to generate, just do it!
+    // 2. One image per function call. Create only 1 image per request unless you got more than 1 url then display all.
+    // 3. Always end any prompt with the extensions "--v 6 --fast" to make sure that you use version 6 of the model midjurney and on the fast mode.
+    // 4. Create only one prompt, Do only one function call, Then display all the image you get.
+    // 5. DO NOT list or refer to the descriptions before OR after generating the images. They should ONLY ever be written out ONCE, in the \`"prompts"\` field of the request. You do not need to ask for permission to generate, just do it!
     `;
     this.schema = z.object({
       prompt: z
