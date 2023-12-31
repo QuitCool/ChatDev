99fce5759585637367a137878db85a19fb8a792d
Quitcool <mo7senhajjaj@gmail.com>
2023-12-31 13:21:16 +0200

    push

--- api/app/clients/tools/structured/DALLE3.js
+++ api/app/clients/tools/structured/DALLE3.js
@@ -36,14 +36,10 @@ class DALLE3 extends Tool {
     // 2. 4 image per function call. Create only 4 image per request unless explicitly told to generate multiply of 4 image.
     // 3. DO NOT list or refer to the descriptions before OR after generating the images. They should ONLY ever be written out ONCE, in the \`"prompts"\` field of the request. You do not need to ask for permission to generate, just do it!
     // 4. Always mention the image type (photo, oil painting, watercolor painting, illustration, cartoon, drawing, vector, render, etc.) at the beginning of the caption. Unless the captions suggests otherwise, make one of the images a photo.
-    // 5. Diversify depictions of ALL images with people to always include always DESCENT and GENDER for EACH person using direct terms. Adjust only human descriptions.
-    // 6. if you got 4 images as the result of the API Call, make sure you list the 4 images visualy one after one.
+    // 5. if you got 4 images as the result of the API Call, make sure you list the 4 images visualy one after one.
     // - EXPLICITLY specify these attributes, not abstractly reference them. The attributes should be specified in a minimal way and should directly describe their physical form.
-    // - Your choices should be grounded in reality. For example, all of a given OCCUPATION should not be the same gender or race. Additionally, focus on creating diverse, inclusive, and exploratory scenes via the properties you choose during rewrites.  Make choices that may be insightful or unique sometimes.
-    // - Use "various" or "diverse" ONLY IF the description refers to groups of more than 3 people. Do not change the number of people requested in the original description.
     // - Don't alter memes, fictional character origins, or unseen people. Maintain the original prompt's intent and prioritize quality.
-    // The prompt must intricately describe every part of the image in concrete, objective detail. THINK about what the end goal of the description is, and extrapolate that to what would make satisfying images.
-    // All descriptions sent to Midjourney should be a paragraph of text that is extremely descriptive and detailed. Each should be more than 3 sentences long.`;
+    // All descriptions sent to Midjourney should be a paragraph of text that is fixation of the main prompt with the same length. Each should be more than 3 words long.`;
     this.schema = z.object({
       prompt: z
         .string()
