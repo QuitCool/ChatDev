5598fc5fa7240254ded68db0aab72294e2c4cb1e
Quitcool <mo7senhajjaj@gmail.com>
2024-01-01 15:26:35 +0200

    mdjjjj

--- api/app/clients/tools/structured/DALLE3.js
+++ api/app/clients/tools/structured/DALLE3.js
@@ -44,8 +44,7 @@ class DALLE3 extends Tool {
     // - Don't alter memes, fictional character origins, or unseen people. Maintain the original prompt's intent and prioritize quality.
     // The prompt must intricately describe every part of the image in concrete, objective detail. THINK about what the end goal of the description is, and extrapolate that to what would make satisfying images.
     // All descriptions sent to dalle should be a paragraph of text that is extremely descriptive and detailed. Each should be more than 10 words long.
-    // MAKE SURE that the end result is one prompt ready to send to dalle and that prompt contains the main given prompt (fixed) and 3 or 4 short descriptions (2 words) after it with ',' after each one.
-    // DON'T add new ideas to the descriptions after the given prompt, instead of that add enhance descriptions or arguments that relate to the given prompt not new things.`;
+    // MAKE SURE that the end result is one prompt ready to send to dalle and that prompt contains the main given prompt (fixed) and 3 or 4 short descriptions (2 words) after it with ',' after each one.`;
     this.schema = z.object({
       prompt: z
         .string()
