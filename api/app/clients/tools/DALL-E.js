bda921a3e8a116aeb2d15a70253b014a5d08ce9b
Quitcool <mo7senhajjaj@gmail.com>
2024-01-10 14:39:05 +0200

    test highly usage

--- api/app/clients/tools/DALL-E.js
+++ api/app/clients/tools/DALL-E.js
@@ -94,7 +94,7 @@ class OpenAICreateImage extends Tool {
         resp = await this.openai.images.generate({
           model,
           prompt: this.replaceUnwantedChars(prompt),
-          n: 1,
+          n: 4,
         });
         break; // If the image generation is successful, break out of the loop
       } catch (error) {
