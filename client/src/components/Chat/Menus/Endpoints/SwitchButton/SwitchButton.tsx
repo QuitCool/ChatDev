a588c16454e5db6427ab47ec170803422b5651c5
QuitCool <mo7senhajjaj@gmail.com>
2024-01-25 10:55:25 +0200

    go

--- client/src/components/Chat/Menus/Endpoints/SwitchButton/SwitchButton.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { useRecoilState } from 'recoil';
-import { EModelEndpoint } from 'librechat-data-provider';
-import { useChatContext } from '~/Providers';
-
-const SwitchButton = () => {
