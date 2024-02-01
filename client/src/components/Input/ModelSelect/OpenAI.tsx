d168f27ce350202d54f120a5b67de0f44d6614ea
QuitCool <mo7senhajjaj@gmail.com>
2024-02-01 04:18:28 +0000

    aa

--- client/src/components/Input/ModelSelect/OpenAI.tsx
+++ client/src/components/Input/ModelSelect/OpenAI.tsx
@@ -4,11 +4,19 @@ import type { TModelSelectProps } from '~/common';
 
 export default function OpenAI({ conversation, setOption, models, showAbove = true, popover = true }: TModelSelectProps) {
   const modelDisplayNames = {
-    'gpt-4-1106-preview': 'GPT 4',
     'gpt-4-turbo-preview': 'GPT 4 (Turbo)',
+    'gpt-4-1106-preview': 'GPT 4',
+    'gpt-4-0613': 'GPT 4 (06/13)',
     'gpt-4-vision-preview': 'GPT 4 (Vision)',
-    'gpt-4-0613': 'GPT 4 (Old Model)',
-    'gpt-3.5-turbo-1106': 'GPT 3.5',
+    'gpt-3.5-turbo-1106': 'GPT 3.5 (11/06)',
+    'claude-2': 'Claude 2 (PRO)',
+    'claude-instant': 'Claude 1 (FREE)',
+    'gemini-pro': 'Gemini (PRO)',
+    'gemini-pro-vision' :'Gemini (Vision)',
+    'llama-2-70b-chat': 'LLaMA 2 (70 Billion)',
+    'llama-2-13b-chat': 'LLaMA 2 (13 Billion)',
+    'llama-2-7b-chat': 'LLaMA 2 (7 Billion)',
+    'mixtral-8x7b': 'Mistral (8x7 Billion)',
   };
 
   const getModelDisplayName = (modelValue) => modelDisplayNames[modelValue] || modelValue;
